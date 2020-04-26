743817c7e9d9b26c9df3a83469f0d888b2b4ba14
chalosalvador <chalosalvador@gmail.com>
2020-04-26 12:46:23 -0500

    Authorization

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,12 @@ use Illuminate\Http\Request;
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 
-Route::get('articles', 'ArticleController@index');
 
 
     Route::get('articles/{article}/image', 'ArticleController@image');
 
 Route::group(['middleware' => ['jwt.verify']], function () {
+Route::get('articles', 'ArticleController@index');
     Route::get('user', 'UserController@getAuthenticatedUser');
 
     // ARTICLES
